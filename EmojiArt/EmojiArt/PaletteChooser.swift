fdb5caf73fc7b49f5208f95d3b9721ac9506963c
Eman <prof.eman2020@gmail.com>
2020-09-01 04:37:22 +0200

    Added Demo Code of lec9

---
+++ EmojiArt/EmojiArt/PaletteChooser.swift
@@ -0,0 +1,21 @@
+//
+//  PaletteChooser.swift
+//  EmojiArt
+//
+//  Created by Eman on 9/1/20.
+//  Copyright © 2020 CS193p Stanford 2020. All rights reserved.
+//
+
+import SwiftUI
+
+struct PaletteChooser: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PaletteChooser_Previews: PreviewProvider {
+    static var previews: some View {
+        PaletteChooser()
+    }
+}
