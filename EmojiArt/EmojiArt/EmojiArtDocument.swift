605803c0347e042c011b3e82d7e505bfef93ae3f
Eman <prof.eman2020@gmail.com>
2020-09-02 04:18:31 +0200

    Added Demo Code for lec 10

--- EmojiArt/EmojiArt/EmojiArtDocument.swift
+++ EmojiArt/EmojiArt/EmojiArtDocument.swift
@@ -9,25 +9,40 @@
 import SwiftUI
 import Combine
 
-class EmojiArtDocument: ObservableObject {
+class EmojiArtDocument: ObservableObject, Hashable, Identifiable {
+    
+    static func == (lhs: EmojiArtDocument , rhs: EmojiArtDocument ) -> Bool {
+        lhs.id == rhs.id
+    }
+    
+    var id = UUID() // let ?
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+    
+    
     static let palette: String = "⭐️🌨🍎🌎🥨⚾️"
     
    @Published private var emojiArt: EmojiArt
     
-    private static let untitled = "EmojiArtDocument.Unitled"
     
     private var autosaveCancellable: AnyCancellable?
     
-    init() {
-        emojiArt = EmojiArt(json: UserDefaults.standard.data(forKey: EmojiArtDocument.untitled)) ?? EmojiArt()
+    init(id: UUID? = nil) {
+        self.id = id ?? UUID()
+        let defaultsKey = "EmojiArtDocument.\(self.id.uuidString)"
+        emojiArt = EmojiArt(json: UserDefaults.standard.data(forKey: defaultsKey)) ?? EmojiArt()
         autosaveCancellable = $emojiArt.sink { emojiArt in
-            UserDefaults.standard.set(emojiArt.json, forKey: EmojiArtDocument.untitled)
+            UserDefaults.standard.set(emojiArt.json, forKey: defaultsKey)
         }
         
         fetchBackgroundImageData()
     }
     
     @Published private(set) var backgroundImage: UIImage?
+    @Published var steadyStatePanOffset: CGSize = .zero
+    @Published var steadyStateZoomScale: CGFloat = 1.0
     
     var emojis: [EmojiArt.Emoji] {emojiArt.emojis}
     
--- EmojiArt/EmojiArt/PaletteChooser.swift
+++ EmojiArt/EmojiArt/PaletteChooser.swift
@@ -12,6 +12,7 @@ struct PaletteChooser: View {
     @ObservedObject var document: EmojiArtDocument
     
     @Binding var chosenPalette: String
+    @State private var showPaletteEditor = false
     
     var body: some View {
         HStack {
@@ -20,10 +21,82 @@ struct PaletteChooser: View {
             }, onDecrement: {
                 self.chosenPalette = self.document.palette(before: self.chosenPalette)
             }, label: {EmptyView()})
+            Text(self.document.paletteNames[self.chosenPalette] ?? "") // lec9
+            Image(systemName: "keyboard").imageScale(.large)
+                .onTapGesture {
+                    self.showPaletteEditor = true
+            }
+            .sheet(isPresented: $showPaletteEditor) {
+                PaletteEditor(chosenPalette: self.$chosenPalette,isShowing: self.$showPaletteEditor)
+                    .environmentObject(self.document)
+                    .frame(minWidth: 300, minHeight: 500)
+            }
         }
         .fixedSize(horizontal: true, vertical: false)
     }
 }
+
+struct PaletteEditor: View {
+    @Binding var chosenPalette: String
+    @Binding  var isShowing: Bool
+    @EnvironmentObject var document: EmojiArtDocument
+    @State private var paletteName: String = ""
+    @State private var emojisToAdd: String = ""
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            ZStack{
+            Text("Palette Editor").font(.headline).padding()
+            HStack {
+                Spacer()
+                Button( action: {
+                    self.isShowing = false
+                    }, label:  {Text ("Done") }).padding()
+              }
+           }
+            Divider()
+            Form {
+                Section{
+                    TextField("Palette Name", text:  $paletteName, onEditingChanged: { began in
+                         if !began {
+                            self.document.renamePalette(self.chosenPalette, to: self.paletteName)
+                        }
+                    })
+                    
+                    TextField("Add Emoji", text:  $emojisToAdd, onEditingChanged: { began in
+                                               if !began {
+                                                    self.chosenPalette = self.document.addEmoji(self.emojisToAdd, toPalette: self.chosenPalette)
+                                                       self.emojisToAdd = ""
+                       }
+                   })
+                }
+               
+                Section(header: Text("Remove Emoji")){
+                        Grid(chosenPalette.map { String($0) }, id: \.self) { emoji in
+                            Text(emoji).font(Font.system(size: self.fontSize))
+                                .onTapGesture {
+                                    self.chosenPalette = self.document.removeEmoji(emoji, fromPalette: self.chosenPalette)
+                            }
+                        }
+                        .frame(height: self.height)
+                }
+                  
+            }
+        }
+        .onAppear{self.paletteName = self.document.paletteNames[self.chosenPalette] ?? ""}
+    }
+    
+    // MARK: - Drawing Constants
+    
+    var height: CGFloat {
+        CGFloat ((chosenPalette.count - 1 )/6) * 70 + 70
+    }
+    let fontSize: CGFloat = 40
+}
+
+
+
+
 
 struct PaletteChooser_Previews: PreviewProvider {
     static var previews: some View {
